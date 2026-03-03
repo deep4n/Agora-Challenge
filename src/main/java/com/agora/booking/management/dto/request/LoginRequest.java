1d92f7e628d1ad3123883dfb1edb8f91a57706c4
mdyfann <dfnrmdhn02@gmail.com>
2026-03-04 00:00:18 +0700

    feat(FR02): add LoginRequest DTO with email and password validation

---
+++ src/main/java/com/agora/booking/management/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.agora.booking.management.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
