0e453abddd188ca463483e7285df14011cccc50a
mdyfann <dfnrmdhn02@gmail.com>
2026-03-04 00:01:10 +0700

    feat(FR02): add login method to AuthService interface

--- src/main/java/com/agora/booking/management/service/AuthService.java
+++ src/main/java/com/agora/booking/management/service/AuthService.java
@@ -1,9 +1,13 @@
 package com.agora.booking.management.service;
 
+import com.agora.booking.management.dto.request.LoginRequest;
 import com.agora.booking.management.dto.request.RegisterRequest;
+import com.agora.booking.management.dto.response.LoginResponse;
 import com.agora.booking.management.dto.response.UserResponse;
 
 public interface AuthService {
 
     UserResponse register(RegisterRequest request);
+
+    LoginResponse login(LoginRequest request);
 }
