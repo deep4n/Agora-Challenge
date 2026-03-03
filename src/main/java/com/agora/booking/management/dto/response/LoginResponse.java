537c2c9c462395a96273b9b4da1bd251c7136806
mdyfann <dfnrmdhn02@gmail.com>
2026-03-04 00:00:25 +0700

    feat(FR02): add LoginResponse DTO with accessToken, tokenType, and user

---
+++ src/main/java/com/agora/booking/management/dto/response/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.agora.booking.management.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String accessToken;
+    private String tokenType;
+    private UserResponse user;
+}
