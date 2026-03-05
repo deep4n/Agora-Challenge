40d346f7626c103c5d344f94865b3c8032879303
mdyfann <dfnrmdhn02@gmail.com>
2026-03-06 04:20:36 +0700

    feat(FR11): add getMyBookings method to BookingService interface

--- src/main/java/com/agora/booking/management/service/BookingService.java
+++ src/main/java/com/agora/booking/management/service/BookingService.java
@@ -3,7 +3,11 @@ package com.agora.booking.management.service;
 import com.agora.booking.management.dto.request.CreateBookingRequest;
 import com.agora.booking.management.dto.response.BookingResponse;
 
+import java.util.List;
+
 public interface BookingService {
 
     BookingResponse createBooking(CreateBookingRequest request, String userEmail);
+
+    List<BookingResponse> getMyBookings(String userEmail);
 }
