a57b730933faddb522ea6a696a91e7bcb18405a9
mdyfann <dfnrmdhn02@gmail.com>
2026-03-06 01:49:07 +0700

    feat(FR05): update EventRepository to extend JpaSpecificationExecutor

--- src/main/java/com/agora/booking/management/repository/EventRepository.java
+++ src/main/java/com/agora/booking/management/repository/EventRepository.java
@@ -2,9 +2,11 @@ package com.agora.booking.management.repository;
 
 import com.agora.booking.management.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long>,
+        JpaSpecificationExecutor<Event> {
 
 }
