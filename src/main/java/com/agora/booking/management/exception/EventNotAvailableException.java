8c06833cdbd02f4dc32e8ae32c4a30e8a66a6958
mdyfann <dfnrmdhn02@gmail.com>
2026-03-06 04:10:40 +0700

    feat(FR10): add EventNotAvailableException for inactive or past event booking attempts

---
+++ src/main/java/com/agora/booking/management/exception/EventNotAvailableException.java
@@ -0,0 +1,8 @@
+package com.agora.booking.management.exception;
+
+public class EventNotAvailableException extends RuntimeException {
+
+    public EventNotAvailableException(String message) {
+        super(message);
+    }
+}
